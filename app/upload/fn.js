342a0d2d7ced7e859c70c950a32cff080ca59d5c
Bharath K <148028778+Bharathh-k@users.noreply.github.com>
2025-02-16 07:00:04 +0530

    Create fn.js

---
+++ app/upload/fn.js
@@ -0,0 +1,21 @@
+function asd(count) {
+    functionB(count-1)
+    qwerty()
+}
+
+function functionB(count) {
+    console.log("Function B called with count:", count);
+    a()
+}
+
+
+
+function qwerty() {
+    for (let i = 0; i < 2; i = i + 1){
+        let a = 10;
+    }
+}
+
+
+asd(4);
+
