8a714c47b2194ad0e997968a332eda9b502a5ab5
Chirag A Reddy <78217806+chiragintech@users.noreply.github.com>
2025-08-01 23:21:05 +0530

    clean 2

--- app/rag_client.py
+++ app/rag_client.py
@@ -1,22 +1,22 @@
-const readline = require("readline");
-const axios = require("axios");
+# const readline = require("readline");
+# const axios = require("axios");
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+# const rl = readline.createInterface({
+#     input: process.stdin,
+#     output: process.stdout
+# });
 
-const askQuestion = async () => {
-    rl.question("Enter your question: ", async (question) => {
-        try {
-            const response = await axios.post("http://localhost:8080/ask", { question });
-            console.log("\nAI Answer:\n", response.data.answer);
-        } catch (error) {
-            console.error("Error:", error.response ? error.response.data : error.message);
-        }
-        rl.close();
-    });
-};
+# const askQuestion = async () => {
+#     rl.question("Enter your question: ", async (question) => {
+#         try {
+#             const response = await axios.post("http://localhost:8080/ask", { question });
+#             console.log("\nAI Answer:\n", response.data.answer);
+#         } catch (error) {
+#             console.error("Error:", error.response ? error.response.data : error.message);
+#         }
+#         rl.close();
+#     });
+# };
 
-// Run the client
-askQuestion();
+# // Run the client
+# askQuestion();
